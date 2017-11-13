5ac22fe72566462f427468aec3822949b87e483d
Ankur Lathwal <alathw2@uic.edu>
2017-11-12 21:28:06 -0600

    Installed Bootstrap, ngMessages. Applied stylings.

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-var app = angular.module("App",["ngRoute"]);
+var app = angular.module("App",["ngRoute","ngMessages"]);
 
 
--- Controllers.js
+++ Controllers.js
@@ -8,17 +8,24 @@ app.controller("userTableCtrl",function($scope, $http,$location,userService){
     });
 });
 
-app.controller("userCtrl",function($scope,userService){
+app.controller("userCtrl",function($scope,userService,$http,$location){
     $scope.user = userService.getCurrentUser();
+    $scope.deleteUser = function(){
+        $http.delete("http://mocker.egen.io/users/" + $scope.user.id).then(function(response){
+            console.log(response.statusText);
+            $location.path("/");
+        });
+    };
 });
 
-app.controller("addUserCtrl",function($scope,$http){
+app.controller("addUserCtrl",function($scope,$http,$location){
     $scope.submit = function(){
         var data = $scope.fields;
         console.log(data);
+        data.profilePic = "images/avatar.png";
         $http.post("http://mocker.egen.io/users",data).then(function(response){
             console.log("POST response :" + response.statusText);
-            
+                $location.path("/");
         });
     };
 });
