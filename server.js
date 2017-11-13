0b8eb20266d25faf7bacde3b02aa1655db638a54
Ankur Lathwal <alathw2@uic.edu>
2017-11-12 22:54:25 -0600

    Updated server config for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
   
 app.use(express.static(__dirname + '/'));
  
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
   const host = server.address().address;
   const port = server.address().port;
 });
