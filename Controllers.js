bc158eee865b5db11b9b6528da1686f5ab58e5a7
Ankur Lathwal <alathw2@uic.edu>
2017-11-12 23:00:11 -0600

    https

--- Controllers.js
+++ Controllers.js
@@ -1,5 +1,5 @@
 app.controller("userTableCtrl",function($scope, $http,$location,userService,messageService){
-    $http.get("http://mocker.egen.io/users").then(function(response){
+    $http.get("https://mocker.egen.io/users").then(function(response){
         $scope.data = response.data;
         $scope.view = function(user){
             userService.setCurrentUser(user);
@@ -17,7 +17,7 @@ app.controller("userTableCtrl",function($scope, $http,$location,userService,mess
 app.controller("userCtrl",function($scope,userService,$http,$location,messageService){
     $scope.user = userService.getCurrentUser();
     $scope.deleteUser = function(){
-        $http.delete("http://mocker.egen.io/users/" + $scope.user.id).then(function(response){
+        $http.delete("https://mocker.egen.io/users/" + $scope.user.id).then(function(response){
             console.log(response.statusText);
             messageService.setCurrentMessage("User deleted successfully");             
             $location.path("/notify");
@@ -33,7 +33,7 @@ app.controller("addUserCtrl",function($scope,$http,$location,messageService){
         var data = $scope.fields;
         console.log(data);
         data.profilePic = "images/avatar.png";
-        $http.post("http://mocker.egen.io/users",data).then(function(response){
+        $http.post("https://mocker.egen.io/users",data).then(function(response){
             console.log("POST response :" + response.statusText);
             messageService.setCurrentMessage("User added successfully");             
             $location.path("/notify");
